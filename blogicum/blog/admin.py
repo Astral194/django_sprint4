30a31a9b6b308b82a08f6d1c3528b9e8a6c0b8b0
Astral <pegovn6@gmail.com>
2025-05-31 01:41:26 +0300

    попытка намбер ван

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from .models import Category, Location, Post
+
+
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 0
+    show_change_link = True
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (PostInline,)
+    list_display = ('title', 'is_published')
+    list_editable = ('is_published',)
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    list_filter = ('is_published',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published')
+    list_editable = ('is_published',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_filter = ('is_published',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'pub_date',
+        'is_published',
+        'author',
+        'category',
+        'location'
+    )
+    list_editable = ('is_published',)
+    list_display_links = ('title',)
+    search_fields = ('title', 'text')
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Post, PostAdmin)
+
+admin.site.empty_value_display = 'Не задано'
